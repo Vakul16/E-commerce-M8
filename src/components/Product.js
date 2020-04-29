30cb4743cdb8cba1d28b27b93a66e2d1ae3ef693
vakul16 <valusavakhul@gmail.com>
2020-04-29 12:55:07 +0530

    changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import {ProductConsumer } from "../context";
+import PropTypes from 'prop-types'
 
 export default class Product extends Component {
     render(){
@@ -46,7 +47,15 @@ export default class Product extends Component {
         )
     }
 }
-
+Product.propTypes = {
+    product : PropTypes.shape({
+        id : PropTypes.number,
+        img : PropTypes.string,
+        title : PropTypes.string,
+        price : PropTypes.number,
+        inCart : PropTypes.bool
+    }).isRequired
+};
 
 const ProductWrapper = styled.div`
 .card {
@@ -76,5 +85,26 @@ const ProductWrapper = styled.div`
 }
 .img-container:hover .card-img-top {
     transform : scale(1.2);
+}
+.card-btn {
+    position : absolute;
+    bottom : 0;
+    right : 0;
+    padding : 0.2rem 0.4rem;
+    background : var(--lightBlue);
+    border : none;
+    color : var(--mainWhite);
+    font-size :1.4rem;
+    border-radius : 0.5rem 0 0 0;
+    transform : translate(100%,100%);
+    transition : all 1s linear;
+}
+.img-container : hover .cart-btn {
+    transform : translate(0, 0);
+
+}
+.car-btn : hover {
+    color  : var(--mainWhite);
+    cursor : pointer;
 }
 `;
